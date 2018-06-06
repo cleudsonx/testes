cb2fdbde403ac7ff79f382319bc41b4e8a6a4f10
Cleudson Cavalcante <sempreonlinedf@gmail.com>
2018-06-06 10:25:27 -0300

    Atualização 6.1
    formatação

--- confirma.py
+++ confirma.py
@@ -4,7 +4,7 @@
 usuario = "rafael"
 password = "1234"
 
-login = raw_input ("Digite seu login de usu�rio: ")
+login = raw_input ("Digite seu login de usuário: ")
 senha = raw_input ("Digite sua senha: ")
 
 if (login == usuario) and (senha == password):
